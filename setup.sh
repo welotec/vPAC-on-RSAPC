aec6f30a11c0ae4ba27a56c79f39805618f9fff3
Daniel Knüppe <d.knueppe@welotec.com>
2026-01-06 15:15:31 +0100

    added setup.sh for local installation convinience
    Signed-off-by: Daniel Knüppe <d.knueppe@welotec.com>

---
+++ setup.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+apt install -y ansible
+
+read IFACE GATEWAY <<<"$(
+  ip route show default | awk '{print $5, $3}'
+)"
+
+IP_ADDR=$(
+  ip -4 addr show dev "$IFACE" \
+  | awk '/inet / {print $2}' \
+  | cut -d/ -f1
+)
+
+DNS=$(
+  resolvectl dns "$IFACE" 2>/dev/null \
+  | awk 'NR>1 {for (i=2;i<=NF;i++) printf "%s ", $i}'
+)
+
+# Fallback if resolvectl returns nothing
+[ -z "$DNS" ] && DNS=$(awk '/^nameserver/ {printf "%s ", $2}' /etc/resolv.conf)
+
+echo "Interface: $IFACE"
+echo "IP:        $IP_ADDR"
+echo "Gateway:   $GATEWAY"
+echo "DNS:       $DNS"
+
+ansible-playbook -l localhost install_packages_deb.yml
+
+# I haven't figured out why 
+#ip address add $IP_ADDR dev $IFACE
+#ip route add default via $GATEWAY dev $INTERFACE
+resolvectl dns $IFACE $DNS
+ansible-playbook -l localhost install_packages_deb.yml
+ansible-playbook -l localhost start_timemaster.yml
+ansible-playbook -l localhost setup_tuned.yml
+ansible-playbook -l localhost assigne_interfaces.yml
+ansible-playbook -l localhost switch_NM_to_systemd-networkd.yml
+ansible-playbook -l localhost download_ssc600.yml
+echo "reboot the machine and run the following two commands:"
+echo "ansible-playbook -l localhost download_ssc600.yml"
+echo "ansible-playbook -l localhost install_ssc600.yml"
+
